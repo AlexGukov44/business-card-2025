d17eeb1c91891b8b2f2a23d894c07ab0cf1e54cd
Александр Жуков <alexrabota44@mail.ru>
2025-06-12 23:44:47 +0300

    swiper

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,18 @@
-const carouselInner = document.querySelector('.carousel-inner');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const carouselWidth = document.querySelector('.carousel').offsetWidth;
-let currentPosition = 0;
-const numItems = carouselItems.length;
-const totalWidth = carouselWidth * numItems; // Общая ширина всех элементов
-
-
-function moveCarousel(offset) {
-  currentPosition += offset;
-
-  // Логика зацикливания:
-  if (currentPosition < 0) {
-    currentPosition = numItems - 1;
-    carouselInner.style.transition = 'none'; // Убираем переход для мгновенного перескока
-    carouselInner.style.transform = `translateX(-${currentPosition * carouselWidth}px)`;
-    setTimeout(() => {
-      carouselInner.style.transition = 'transform 0.5s ease-in-out'; // Восстанавливаем переход
-    }, 10);
-  } else if (currentPosition >= numItems) {
-    currentPosition = 0;
-    carouselInner.style.transition = 'none'; // Убираем переход для мгновенного перескока
-    carouselInner.style.transform = `translateX(0px)`;
-    setTimeout(() => {
-      carouselInner.style.transition = 'transform 0.5s ease-in-out'; // Восстанавливаем переход
-    }, 10);
-  } else {
-    carouselInner.style.transform = `translateX(-${currentPosition * carouselWidth}px)`;
-  }
-}
-
-
-setInterval(() => moveCarousel(1), 3000); // Автопрокрутка каждые 3 секунды
+let swiper = new Swiper(".mySwiper", {
+      slidesPerView:3,
+      spaceBetween: 30,
+      loop: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+      },
+      effect: "coverflow",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+    });
